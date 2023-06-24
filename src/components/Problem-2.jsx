bd7b17d3bdd2b4c715840049bcaed92b070af6fa
kaziFahmid <nirjhorislam42@gmail.com>
2023-06-25 05:44:08 +0600

    problem 2

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
 
 const Problem2 = () => {
+  const [openAllContactsModal, setOpenAllContactsModal] = useState(false);
+  const [openUSContactsModal, setOpenUSContactsModal] = useState(false);
 
-    return (
+  const onOpenAllContactsModal = () => setOpenAllContactsModal(true);
+  const onCloseAllContactsModal = () => setOpenAllContactsModal(false);
+  const onOpenUSContactsModal = () => setOpenUSContactsModal(true);
+  const onCloseUSContactsModal = () => setOpenUSContactsModal(false);
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
+  return (
+    <>
+      <div className="container">
+        <div className="row justify-content-center mt-5">
+          <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+
+          <div className="d-flex justify-content-center gap-3">
+            <button className="btn btn-lg btn-outline-primary" type="button" onClick={onOpenAllContactsModal}>
+              All Contacts
+            </button>
+            <button className="btn btn-lg btn-outline-warning" type="button" onClick={onOpenUSContactsModal}>
+              US Contacts
+            </button>
+          </div>
         </div>
-    );
+      </div>
+
+      <Modal open={openAllContactsModal} onClose={onCloseAllContactsModal} center>
+        <h2>Modal A</h2>
+ 
+      </Modal>
+
+      <Modal open={openUSContactsModal} onClose={onCloseUSContactsModal} center>
+      <h2>Modal B</h2>
+      
+      </Modal>
+    </>
+  );
 };
 
 export default Problem2;
